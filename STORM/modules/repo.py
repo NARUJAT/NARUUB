6e89de7700f12fd5f5fb9a74716e0af883b8dabd
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <165283541+NARUJAT@users.noreply.github.com>
2024-03-29 06:26:25 +0530

    Update repo.py

--- STORM/modules/repo.py
+++ STORM/modules/repo.py
@@ -4,13 +4,13 @@ from config import SUDO_USERS
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["repo"], ["."]))
 async def repo(client, message):
     msg = f"""
-    ** ꜱᴛᴏʀᴍ ᴜꜱᴇʀʙᴏᴛ **
+    ** ɴᴀʀᴜᴊᴀᴀᴛ ᴜꜱᴇʀʙᴏᴛ **
 
-    • **ɢɪᴛʜᴜʙ** » [click here](https://github.com/VARC9210/STORM-USERBOT) 
-    • **ꜱᴜᴘᴘᴏʀᴛ** » [click here](https://t.me/STORM_CHATZ) 
-    • **ᴜᴘᴅᴀᴛᴇꜱ** » [click here](https://t.me/STORM_TECHH)
-    • **ᴅᴇᴠᴇʟᴏᴘᴇʀ** » [click here](https://t.me/kexx_xd)
+    • **ʀᴇᴘᴏ** » [click here](https://te.legra.ph/file/ebc3fc421b8776e29ad98.mp4) 
+    • **ꜱᴜᴘᴘᴏʀᴛ** » [click here](https://t.me/shayrigalibki) 
+    • **ᴜᴘᴅᴀᴛᴇꜱ** » [click here](https://t.me/brokenshayri1)
+    • **ᴅᴇᴠᴇʟᴏᴘᴇʀ** » [click here](https://t.me/mr_naru)
     
-    **ʙʏ @kexx_xd**
+    **ʙʏ @mr_naru**
     """
     await message.edit(msg)
